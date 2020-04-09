f09bab147a0d467acaa453b4cc5baacd54298231
John Hunt <jeh@midmarsh.co.uk>
2020-04-09 16:57:59 +0100

    Example of using inner classes and packages

---
+++ 06-inner-classes/src/com/jjh/bookshop/data/Author.java
@@ -0,0 +1,43 @@
+package com.jjh.bookshop.data;
+
+public class Author implements PrettyPrinter {
+	
+	private String name;
+	private Address address;
+
+	public Author(String name) {
+		this.name = name;
+	}
+	
+	public Author(String name, Address address) {
+		this.name = name;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Author [name=%s, address=%s]", name, address);
+	}
+
+	@Override
+	public void prettyPrint() {
+		System.out.println(String.format("Author [name=%s, address=%s]", name, address));
+	}
+
+}
