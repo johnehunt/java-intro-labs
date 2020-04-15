cf0e615559dea3abae55ffa06f2f1fb7e1e5a11f
John Hunt <jeh@midmarsh.co.uk>
2020-04-15 18:15:35 +0100

    Added further inheritance

--- 04-abstract/src/BookshopApp.java
+++ 04-abstract/src/BookshopApp.java
@@ -5,10 +5,10 @@ public class BookshopApp {
 		System.out.println("Welcome to the Java Bookshop");
 		
 		Address authorAddress = new Address(10, "High Street", "Any Town", "Somerset", "SA1 23Z");
-		Author author = new Author("Pete Smith", authorAddress);
+Author author = new Author("Pete Smith", authorAddress, "Technical");
 		
 		Address publisherAddress = new Address(1, "Main Street", "Some City", "Kent", "KA1 43A");
-		Publisher publisher = new Publisher("Tech Books Publishing Ltd.", publisherAddress);
+		Publisher publisher = new Publisher("Biull Smith", publisherAddress, "Tech Books Publishing Ltd.");
 		
 		Book book = new Book("Java Unleashed", author, publisher, 15.95);
 		System.out.println("Book: " + book);
