c3f21591dcf27a2f6794010f7ef74bd7c9f5959c
John Hunt <jeh@midmarsh.co.uk>
2020-04-16 16:49:06 +0100

    Fixed typo in string

--- 04-abstract/src/BookshopApp.java
+++ 04-abstract/src/BookshopApp.java
@@ -5,10 +5,10 @@ public class BookshopApp {
 		System.out.println("Welcome to the Java Bookshop");
 		
 		Address authorAddress = new Address(10, "High Street", "Any Town", "Somerset", "SA1 23Z");
-Author author = new Author("Pete Smith", authorAddress, "Technical");
+        Author author = new Author("Pete Smith", authorAddress, "Technical");
 		
 		Address publisherAddress = new Address(1, "Main Street", "Some City", "Kent", "KA1 43A");
-		Publisher publisher = new Publisher("Biull Smith", publisherAddress, "Tech Books Publishing Ltd.");
+		Publisher publisher = new Publisher("Bill Smith", publisherAddress, "Tech Books Publishing Ltd.");
 		
 		Book book = new Book("Java Unleashed", author, publisher, 15.95);
 		System.out.println("Book: " + book);
@@ -16,6 +16,7 @@ Author author = new Author("Pete Smith", authorAddress, "Technical");
 		System.out.println("\nCalculating the Sales Discount price");
 		book.setSaleDiscount(10.0);
 		System.out.println("Sale price of book: " + book.calculateSalePrice());
+		book = null;
 		
 		Book book2 = new Book("Java For Professionals", author, publisher, 12.55);
 		System.out.println("\nBook2: " + book2);
